807972f3cad67b5ae22a42d37b1b84226cee183a
VictorOmar111 <axacdmx@gmail.com>
2025-06-30 10:59:53 -0600

    Create lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+// src/lib.rs
+
+// Declaración de todos los módulos que componen nuestra librería.
+// main.rs podrá usar las funciones públicas de estos módulos.
+pub mod config;
+pub mod constants;
+pub mod execution;
+pub mod math;
+pub mod multi;
+pub mod paths;
+pub mod pools;
+pub mod provider;
+pub mod simulator;
+pub mod streams;
+pub mod strategy;
+pub mod utils;
